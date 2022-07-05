3bd003982c866c87189ea116f355fe076b2a757e
mtgrinstead <grinchead@yahoo.com>
2022-07-05 19:30:09 -0400

    updated buttons

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const D8number = Math.trunc(Math.random()*8) + 1;
 const D10number = Math.trunc(Math.random()*10) + 1;
 const D12number = Math.trunc(Math.random()*12) + 1;
 const D20number = Math.trunc(Math.random()*20) + 1;
+
   
 function D4roll() {
     const D4number = Math.trunc(Math.random() * 4) + 1;
@@ -40,4 +41,13 @@ function D12roll() {
 function D20roll() {
     const D20number = Math.trunc(Math.random() * 20) + 1;
     document.getElementById('D20in').value = D20number;
+}
+
+function reset() {
+    document.getElementById('D4in').value = '';
+    document.getElementById('D6in').value = '';
+    document.getElementById('D8in').value = '';
+    document.getElementById('D10in').value = '';
+    document.getElementById('D12in').value = '';
+    document.getElementById('D20in').value = '';
 }
